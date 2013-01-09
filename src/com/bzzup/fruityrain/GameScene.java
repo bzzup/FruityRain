d81dd2b8cce9f161dd497946df2a7164ffefac03
Andrei <Andrei@bzzup>
2013-01-09 15:38:40 -0430

    Drag-n-Drop

--- src/com/bzzup/fruityrain/GameScene.java
+++ src/com/bzzup/fruityrain/GameScene.java
@@ -3,12 +3,14 @@ package com.bzzup.fruityrain;
 import java.util.Random;
 
 import org.andengine.engine.camera.Camera;
+import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.engine.handler.timer.ITimerCallback;
 import org.andengine.engine.handler.timer.TimerHandler;
+import org.andengine.entity.IEntity;
+import org.andengine.entity.modifier.RotationModifier;
+import org.andengine.entity.modifier.ScaleModifier;
 import org.andengine.entity.primitive.Rectangle;
-import org.andengine.entity.scene.IOnAreaTouchListener;
 import org.andengine.entity.scene.IOnSceneTouchListener;
-import org.andengine.entity.scene.ITouchArea;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.sprite.AnimatedSprite;
@@ -20,17 +22,14 @@ import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.util.color.Color;
 
 import android.hardware.SensorManager;
-import android.os.Handler;
-import android.os.Handler.Callback;
-import android.os.Looper;
-import android.os.Message;
+import android.util.Log;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 
-public class GameScene extends Scene implements IOnSceneTouchListener {
+public class GameScene extends Scene implements IOnSceneTouchListener, IUpdateHandler {
 
 	private float dropSpeed = 1f; // sec
 
@@ -71,16 +70,36 @@ public class GameScene extends Scene implements IOnSceneTouchListener {
 		this.attachChild(left);
 		this.attachChild(right);
 
-		this.registerUpdateHandler(this.mWorld);
+		setTouchAreaBindingOnActionDownEnabled(true);
+		setTouchAreaBindingOnActionMoveEnabled(true);
 		
+		this.registerUpdateHandler(sceneUpdateHandler);
+		
+		this.registerUpdateHandler(this.mWorld);
 		createFaceWithPeriod();
 	}
+	
+	IUpdateHandler sceneUpdateHandler = new IUpdateHandler() {
+		
+		@Override
+		public void reset() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void onUpdate(float pSecondsElapsed) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
 
 	@Override
 	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
 		if (this.mWorld != null) {
 			if (pSceneTouchEvent.isActionDown()) {
-//				this.addFace(pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
+				// this.addFace(pSceneTouchEvent.getX(),
+				// pSceneTouchEvent.getY());
 				return true;
 			}
 		}
@@ -120,48 +139,78 @@ public class GameScene extends Scene implements IOnSceneTouchListener {
 		// }
 
 		face.animate(200);
-
+		this.registerTouchArea(face);
 		this.attachChild(face);
 		this.mWorld.registerPhysicsConnector(new PhysicsConnector(face, body, true, true));
-		
+
 	}
-	
+
 	private void createFaceWithPeriod() {
 		TimerHandler timeHandler;
 		timeHandler = new TimerHandler(dropSpeed, true, new ITimerCallback() {
-			
+
 			@Override
 			public void onTimePassed(TimerHandler pTimerHandler) {
 				addFaceAtRandomPosition();
-				
+
 			}
 		});
 		mainActivity.getEngine().registerUpdateHandler(timeHandler);
 	}
 
 	private void addFaceAtRandomPosition() {
-		final AnimatedSprite face;
-		final Body body;
-
-		face = new AnimatedSprite(new Random().nextInt(mainActivity.CAMERA_WIDTH - 10) + 5, 10, mainActivity.mCircleFaceTextureRegion,
-				mainActivity.getVertexBufferObjectManager()){
-			
-			public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
-				this.detachSelf();
-
-				return mFlippedHorizontal;};
-		};
+		final Ball face;
+//		final Body body;
 		
-		body = PhysicsFactory.createCircleBody(mWorld, face, BodyType.DynamicBody, Main.FIXTURE_DEF);
-		face.animate(200);
+		face = new Ball(new Random().nextInt(mainActivity.CAMERA_WIDTH - 10) + 5, 10, mainActivity.mCircleFaceTextureRegion, mainActivity.getVertexBufferObjectManager(), mWorld);
+//		{
+//
+//			@Override
+//			public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
+//				moveToXY(this, pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
+//				return true;
+//			};
+//
+//		};
 
-		this.attachChild(face);
-		this.mWorld.registerPhysicsConnector(new PhysicsConnector(face, body, true, true));
+//		body = PhysicsFactory.createCircleBody(mWorld, face, BodyType.DynamicBody, Main.FIXTURE_DEF);
+//		face.animate(200);
+
+		
+		this.mWorld.registerPhysicsConnector(new PhysicsConnector(face, face.getBody(), true, true));
 		this.registerTouchArea(face);
+		this.setTouchAreaBindingOnActionDownEnabled(true);
+		this.setTouchAreaBindingOnActionMoveEnabled(true);
+		this.attachChild(face);
 	}
-
-
 	
+	private void removeBall(final Ball iBall) {
+		mainActivity.runOnUpdateThread(new Runnable() {
+			
+			@Override
+			public void run() {
+				PhysicsConnector physConnector = mWorld.getPhysicsConnectorManager().findPhysicsConnectorByShape(iBall);
+				mWorld.destroyBody(physConnector.getBody());
+				mWorld.unregisterPhysicsConnector(physConnector);
+				iBall.detachSelf();
+			}
+		});
+	}
 	
-
+	private void explode(final Ball iBall) {
+		iBall.registerEntityModifier(new ScaleModifier(0.2f, 1, 2) {
+			
+			@Override
+			protected void onModifierFinished(IEntity pItem) {
+				removeBall(iBall);
+				super.onModifierFinished(pItem);
+			}
+		}); 
+	}
+	
+//	private void moveToXY(final Ball iBall, float x, float y) {
+//		iBall.setPosition(x - iBall.getWidth()/2, y - iBall.getHeight()/2);
+//	}
+	
+		
 }
--- src/com/bzzup/fruityrain/Main.java
+++ src/com/bzzup/fruityrain/Main.java
@@ -206,6 +206,6 @@ public class Main extends SimpleBaseGameActivity implements IOnSceneTouchListene
 		setCurrentScene(new SplashScreen());
 		return mCurrentScene;
 	}
-
+	
 	
 }
